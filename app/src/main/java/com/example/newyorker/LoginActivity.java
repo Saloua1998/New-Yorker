c2d30f90ccfb2f6e720a0228d80c52fbed0ae283
Saloua <saloua1998@live.nl>
2019-05-21 18:26:52 +0200

    database is linked

--- app/src/main/java/com/example/newyorker/LoginActivity.java
+++ app/src/main/java/com/example/newyorker/LoginActivity.java
@@ -8,35 +8,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText Name;
-    private EditText Password;
+public class LoginActivity extends AppCompatActivity {
+    private EditText Email, Password;
     private Button Login;
-    private int Counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-        Name = findViewById(R.id.name);
+        Email = findViewById(R.id.email);
         Password = findViewById(R.id.password);
         Login = findViewById(R.id.login);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate(Name.getText().toString(), Password.getText().toString());
+                validate(Email.getText().toString(), Password.getText().toString());
             }
         });
     }
 
     private void validate(String userName, String userPassword){
-        if ((userName.equals("Saloua")) && (userPassword.equals("test"))){
-            Intent intent = new Intent(MainActivity.this, Inventory.class);
+        if ((userName.equals("saloua1998@live.nl")) && (userPassword.equals("test"))){
+            Intent intent = new Intent(LoginActivity.this, Inventory.class);
             startActivity(intent);
         }else{
-            Toast.makeText(MainActivity.this, "Verkeer wachtwoord of naam ingevoerd", Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, "Verkeer wachtwoord of naam ingevoerd", Toast.LENGTH_SHORT).show();
         }
     }
 }
